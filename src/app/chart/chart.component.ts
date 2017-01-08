d1a95fdcff1110c24759fecb45fa13d6d44ab19c
Aleksandar <aleksandar@localhost.localdomain>
2017-01-08 19:23:47 +0100

    added values to x axis

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -12,7 +12,7 @@ import { TemperatureChartData } from '../shared/model';
 export class ChartComponent {
   @Input()
   set chartData(data: TemperatureChartData) {
-    console.log('data', data);
+
     this.options.series = data.series.map((s) => {
       let hs: highcharts.IndividualSeriesOptions = {
         name: s.name,
@@ -21,11 +21,19 @@ export class ChartComponent {
       return hs;
     });
 
+    let categories: Array<string> = [];
+    this.options.xAxis = data.series.map((s) => {
+      s.data.map(d => categories.push(d.date))
+    });
+
+    this.options.xAxis.categories = categories;
+
     this.options = Object.assign({}, this.options);
   }
 
   options: highcharts.Options = {
     title: 'Temperature data',
+    labels: [],
   };
 
 }
